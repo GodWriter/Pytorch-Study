8b2f9fae10482425738c569206b3e622828ec695
GodWriter <33643881+GodWriter@users.noreply.github.com>
2017-11-18 23:17:20 +0800

    Update cnn_with_test.py

--- cnn_with_test.py
+++ cnn_with_test.py
@@ -93,3 +93,25 @@ for data in testloader:
 
 print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
 
+
+'''
+假设共有50000条数据,首先我们初始化loss和优化函数
+  >criterion = nn.CrossEntropLoss() #交叉熵损失函数
+  >optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9) #使用SGD(随机梯度下降)优化，学习率为0，001，动量为0.9
+                                                                   #optimizer能保持当前参数状态并基于计算得到的梯度进行更新
+                                                                   #optimizer中被优化的参数必须是Variable,net.parameters是Variable的子类
+卷积训练过程如下：
+1.将50000条小数据分成batch=4的12500份数据，通俗说：将50000条数据，4个一组，分成12500组
+2.现在设置要训练的次数，每次训练的数据是所有的12500组数据，每次训练详情如下：
+  >获取一组数据（共4个）的数据和标签 --> input, labels = data #data的结构是:[4x3x32x32张量, 长度4的张量]
+  >把获得的数据由Tensor转换为Variable --> inputs, labels = Variable(inputs), Variable(labels)
+  >将optimizer中所有的参数置0，因为grad值是累加的，设置为0后，每次反向传播之后grad就会重新更新，而不是累加在上一次的grad的值上。结合上述步骤，
+   每4组数据做一次前向传播，然后计算loss，之后进行反向传播，对所有的参数进行更新。更新公式即梯度下降公式，故每次grad都要在更新之前清0，否则计算
+   出来的梯度会加上上次计算的梯度。 --> optimizers.zero_grad()
+  >做好一切准备之后，进行前向传播，将数据集放到卷积神经网络的入口，数据开始进行前向传播。 -->outputs = net(inputs)
+  >前向传播之后，将得到的结果与正确数据分类标签使用交叉熵计算损失 --> loss = criterion(outputs, labels)
+  >进行反向传播，传播会让每一个网络参数的grad值进行更新，我们网络中每一个参数都是Variable类型，并且均是叶子节点，grad值必会更新 --> loss.backward()
+  >使用SGD更新参数
+ 重复以上步骤，进行多次的训练，确保得到的loss最小
+'''
+
